2ed204624e7f4b8eccae8ddbca5cab5482258e20
TempGaurab <gaurabusa@gmail.com>
2024-11-13 10:45:13 -0500

    a

--- part3.py
+++ part3.py
@@ -15,7 +15,7 @@ def preprocess_image(image_path):
     return image_array
 
 def output(prediction):
-    if prediction > 0.2:
+    if prediction > 0.5:
         return 'Open'
     else:
         return "Closed"
