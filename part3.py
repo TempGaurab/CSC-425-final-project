ec47d40c1e189e75d0f9f2a74eb4d3c6ffb9569f
TempGaurab <gaurabusa@gmail.com>
2024-11-13 23:56:11 -0500

    ZX

--- part3.py
+++ part3.py
@@ -3,7 +3,7 @@ from PIL import Image
 from keras.models import load_model
 
 def get_model():
-    best_model = load_model('bestModel2.keras')
+    best_model = load_model('bestModel2.h5')
     return best_model
 
 def preprocess_image(image_path):
